69939118390e1cc9bc6f29f69947f5e33e010dc4
Med Amine Ck <medamineck4u@gmail.com>
2023-02-15 03:01:12 +0100

    update 15/02/2023

--- js/main.js
+++ js/main.js
@@ -19,35 +19,6 @@ function checkSrollTop() {
   }
 }
 
-//scrolling
-$(document).ready(function () {
-  checkSrollTop();
-  // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        400,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
-});
-
 //send email
 function sendEmail() {
   window.location = "mailto:contact@medamineck.com";
