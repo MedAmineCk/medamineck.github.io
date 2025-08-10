4fedb0b243ff303d61f20f7559208ad45521fc64
MOHAMED-AMINE CHEKROUNI <31735956+MedAmineCk@users.noreply.github.com>
2025-08-10 03:24:10 +0100

    fixing some mobile js and styling

--- js/main.js
+++ js/main.js
@@ -43,3 +43,23 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+/*************mobile header*************/
+
+//menu item clicked
+$('nav a').on('click', function () {
+  className = 'active';
+  $('nav a').removeClass(className);
+  $(this).addClass(className);
+  $('nav').removeClass(className);
+  $('.nav_tab_icon').removeClass('open');
+});
+
+// tap on mobile menu icon
+$('.nav_tab_icon').on('click', function(){
+  $(this).toggleClass('open');
+  $('nav').toggleClass('active')
+})
+
+$('header .contacts').on('click', function(){
+  $(this).toggleClass('active')
+})
